f5fe39e38cb1728399ab9be65909be62bf1cf351
1194 - JULIO HENRIQUE DOS SANTOS <217512@sp-mac-1014-010.local>
2019-11-07 13:59:22 -0300

    implemente methods crud

--- src/app/controllers/PlanController.js
+++ src/app/controllers/PlanController.js
@@ -1,18 +1,59 @@
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+
 class PlanController {
   async index(req, res) {
-    return res.json({ message: 'ok' });
+    const plans = await Plan.findAll();
+    return res.json(plans);
   }
 
   async update(req, res) {
-    return res.json({ message: 'ok' });
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(404).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    const plans = await Plan.findByPk(id);
+
+    if (!plans) {
+      return res.status(404).json({ error: 'Plan do not exists' });
+    }
+
+    await plans.update(req.body);
+    return res.json(plans);
   }
 
   async store(req, res) {
-    return res.json({ message: 'ok' });
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(404).json({ error: 'Validation fails' });
+    }
+
+    const plan = await Plan.create(req.body);
+    return res.json(plan);
   }
 
   async delete(req, res) {
-    return res.json({ message: 'ok' });
+    const { id } = req.params;
+
+    await Plan.destroy({
+      where: {
+        id,
+      },
+    });
+    return res.json({ message: 'Deleted with successful' });
   }
 }
 
